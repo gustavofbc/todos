0e374b4a006b553ac9f35e8a1fe8a6205439205e
Gustavo Felipe <gustavo.felipebc@gmail.com>
2020-10-31 13:06:57 -0300

    conclusao da aula 19 - useState

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import { Todo } from '../models/Todo';
+import { get } from '../services/TodoService';
 import { TodoContextType } from './TodoContextType';
 
 // context é o escopo de variáveis e funções que eu tenho acesso ao longo de todo o programa!
@@ -12,21 +13,23 @@ export const TodoContext = createContext<TodoContextType>({
 });
 
 const TodoProvider = (props: any) => {
-    const todos: Todo[] = [
-        {id: 1, title: 'ir ao supermecado', done: true},
-        {id: 2, title: 'ir a academia', done: false}
-    ];
-    
+    const [todos, setTodos] = useState<Todo[]>(get);
+
     const addTodo = (title: string) => {
-        console.log('Adicionou ' + title);
+        const todo: Todo = { id: todos.length + 1, title: title, done: false}
+        setTodos([...todos, todo])
     }
 
     const removeTodo = (todo: Todo) => {
-        console.log('Removeu ' + todo.title);
+        const index = todos.indexOf(todo);
+        //retorna uma lista de itens poém sem o item cujo atributo passado (index) seja igual ao index da lista.
+        setTodos(todos.filter((_, i) => i !== index))
     }
     
     const toggle = (todo: Todo) => {
-        console.log('Alterou ' + todo.title);
+        const index = todos.indexOf(todo);
+        todos[index].done = !todo.done;
+        setTodos([...todos]);
     }
 
     //o provider é o responsável por distribuir os itens (funções e variáveis) em uma única instância 
