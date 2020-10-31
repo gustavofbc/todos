b0bf95ddf397b050c35bf87ce61d712f561540fd
Gustavo Felipe <gustavo.felipebc@gmail.com>
2020-10-31 13:15:31 -0300

    conclusao da aula 20 - useEffect

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from 'react';
 import { Todo } from '../models/Todo';
-import { get } from '../services/TodoService';
+import { get, save } from '../services/TodoService';
 import { TodoContextType } from './TodoContextType';
 
 // context é o escopo de variáveis e funções que eu tenho acesso ao longo de todo o programa!
@@ -18,18 +18,21 @@ const TodoProvider = (props: any) => {
     const addTodo = (title: string) => {
         const todo: Todo = { id: todos.length + 1, title: title, done: false}
         setTodos([...todos, todo])
+        save(todos);
     }
 
     const removeTodo = (todo: Todo) => {
         const index = todos.indexOf(todo);
         //retorna uma lista de itens poém sem o item cujo atributo passado (index) seja igual ao index da lista.
         setTodos(todos.filter((_, i) => i !== index))
+        save(todos);
     }
     
     const toggle = (todo: Todo) => {
         const index = todos.indexOf(todo);
         todos[index].done = !todo.done;
         setTodos([...todos]);
+        save(todos);
     }
 
     //o provider é o responsável por distribuir os itens (funções e variáveis) em uma única instância 
